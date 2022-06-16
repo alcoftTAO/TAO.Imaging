b301b456494d0f12021b0ad5fb3ba8e057f9e474
Alcoft <101468238+alcoftTAO@users.noreply.github.com>
2022-06-16 22:02:58 +0200

    Add files via upload

---
+++ Images.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TAO.Imaging
+{
+    public static class Images
+    {
+        public static string BitmapToASCII(Bitmap image, DrawType type)
+        {
+            string[] ASCIIChars = new string[]
+            {
+                "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " "
+            };
+            string ASCIIContent;
+
+            bool isSpaceLine = false;
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    int red, green, blue;
+                    red = green = blue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    Color gray = Color.FromArgb(red, green, blue);
+
+                    if (!isSpaceLine)
+                    {
+                        int index = (gray.R * (ASCIIChars.Length - 1)) / 255;
+                        int t = 0;
+
+                        if (type == DrawType.BlackOnWhite)
+                        {
+                            t = index;
+                        }
+                        else if (type == DrawType.WhiteOnBlack)
+                        {
+                            t = ASCIIChars.Length - index - 1;
+                        }
+
+                        sb.Append(ASCIIChars[t]);
+                    }
+                }
+
+                if (!isSpaceLine)
+                {
+                    sb.AppendLine();
+                    isSpaceLine = true;
+                }
+                else
+                {
+                    isSpaceLine = false;
+                }
+            }
+
+            ASCIIContent = sb.ToString();
+            sb = null;
+            image = null;
+
+            return ASCIIContent;
+        }
+
+        public static Bitmap ImageToBitmap(Image img, int width, int height)
+        {
+            return new Bitmap(img, width, height);
+        }
+    }
+
+    public enum DrawType
+    {
+        WhiteOnBlack = 0,
+        BlackOnWhite = 1
+    }
+}
---
+++ Videos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Drawing;
+
+namespace TAO.Imaging
+{
+    public static class Videos
+    {
+        public static void DrawVideoOnConsole(DirectoryInfo info, int FPS, DrawType type)
+        {
+            FileInfo[] files = info.GetFiles();
+            string[] frames = new string[files.Length];
+
+            Console.Clear();
+
+            for (int i = 0; i < frames.Length; i++)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Frame " + i.ToString() + " / " + frames.Length);
+
+                frames[i] = Images.BitmapToASCII(Images.ImageToBitmap(Image.FromFile(files[i].FullName), Console.WindowWidth, Console.WindowHeight), type);
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < frames.Length; i++)
+            {
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write(frames[i]);
+
+                Thread.Sleep(1000 / FPS);
+            }
+        }
+    }
+}
